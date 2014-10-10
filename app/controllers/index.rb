21160e5f46cc63c1cd4eb8953aa452d093208db8
Isabel Rial <isabel.rial@gmail.com>
2014-10-09 23:09:26 -0400

    chart updates with form change

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,12 @@
 get '/' do
-  # Look in app/views/index.erb
+  @imports = IMPORTS.dup
   erb :index
 end
 
 get '/data' do
+	series = params.keys[0]
 	generator = DataGenerator.new
-	data = generator.retrieve_data(LOCALITY_OF_ORIGIN["China"])
+	data = generator.retrieve_data(series)
 	labels = data.keys.reverse
 	values = data.values.reverse
 	[labels, values].to_json
--- public/js/application.js
+++ public/js/application.js
@@ -1,24 +1,56 @@
 $(document).ready(function() {
 	ChartGenerator.context = document.getElementById("myChart").getContext("2d");
+	var series = $("#imports option:selected").val();
+	initializeChart(series);
 
-	$.ajax({
-		  url: "/data",
-		  dataType: "json"
-		})
-		.done(function( data ) {
-    		if ( console && console.log ) {
-      			ChartGenerator.data.labels = data[0]
-      			ChartGenerator.data.datasets[0].data = data[1]
-				ChartGenerator.chart = new Chart(ChartGenerator.context).Line(ChartGenerator.data);
-      		}
-  		});
+	$("#imports").change(function(){
+		var series = $("#imports option:selected").val();
+		updateChart(series);
 
+	})
 
 });
 
+
+var initializeChart = function(series){
+	$.ajax({
+	  url: "/data",
+	  dataType: "json",
+	  data: series
+	})
+	.done(function( data ) {
+		if ( console && console.log ) {
+  			ChartGenerator.data.labels = data[0]
+  			ChartGenerator.data.datasets[0].data = data[1]
+			ChartGenerator.chart = new Chart(ChartGenerator.context).Line(ChartGenerator.data);
+  		}
+		});
+}
+
+var updateChart  = function(series){
+	$.ajax({
+	  url: "/data",
+	  dataType: "json",
+	  data: series
+	})
+	.done(function( data ) {
+		if ( console && console.log ) {
+			ChartGenerator.chart.destroy();
+			ChartGenerator.context = $("#myChart")[0].getContext("2d");
+			ChartGenerator.context.canvas.width = 800;
+			ChartGenerator.context.canvas.height = 400;
+  			ChartGenerator.data.labels = data[0];
+  			ChartGenerator.data.datasets[0].data = data[1];
+  			console.log("destroying!");
+			ChartGenerator.chart = new Chart(ChartGenerator.context).Line(ChartGenerator.data);
+			ChartGenerator.chart.update();
+  		}
+		});
+}
+
 var ChartGenerator = {
 	data: {
-    	labels: ["January", "February", "March", "April", "May", "June", "July","January", "February", "March", "April", "May", "June", "July","January", "February", "March", "April", "May", "June", "July"],
+    	// labels: ["January", "February", "March", "April", "May", "June", "July","January", "February", "March", "April", "May", "June", "July","January", "February", "March", "April", "May", "June", "July"],
     	datasets: [
 	        {
 	            label: "My First dataset",
@@ -28,7 +60,7 @@ var ChartGenerator = {
 	            pointStrokeColor: "#fff",
 	            pointHighlightFill: "#fff",
 	            pointHighlightStroke: "rgba(220,220,220,1)",
-	            data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40]
+	            // data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40]
 	        }//,
 	        // {
 	        //     label: "My Second dataset",
